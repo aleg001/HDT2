7f86347bed0dcdd6c9172aa1a763122bde185250
aleg001 <60187741+aleg001@users.noreply.github.com>
2021-02-02 22:39:09 -0600

    Add files via upload

---
+++ Operaciones.java
@@ -0,0 +1,52 @@
+public class Operaciones implements calculadora{
+
+
+
+    @Override
+    public int suma(int x, int y) {
+        int retorno = x+y;
+        return retorno;
+    }
+
+    @Override
+    public int resta(int x, int y) {
+        int retorno = x-y;
+        return retorno;
+    }
+
+    /** */
+    
+    @Override
+    public int multiplicacion(int x, int y) {
+        int retorno = x*y;
+        return retorno;
+        
+
+    }
+    /** */
+    
+    @Override
+    public int division(int x, int y) {
+        // TODO Auto-generated method stub
+        int retorno = x/y;
+        return retorno;
+    }
+    /** */
+    
+    @Override
+    public int operar(stack x) {
+        // TODO Auto-generated method stub
+        String oper1 = Stack.
+        
+        return 0;
+    }
+    /** */
+    
+
+    @Override
+    public String decode(String a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}

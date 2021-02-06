bae3b5e8a8a3f7f2616ffc1a3933900feb6bef41
aleg001 <60187741+aleg001@users.noreply.github.com>
2021-02-05 22:17:03 -0600

    se agregó el uso de interfaz Stack

---
+++ ImplementacionStack.java
@@ -0,0 +1,45 @@
+/***************************************************************
+* implementacionStack.java
+* Autor: Alejandro Gómez
+* Universidad del Valle de Guatemala
+*
+* Fecha creacion: 05/02/21
+* Ultima modificacion: 5/02/21
+***************************************************************/
+import java.util.ArrayList;
+
+public class ImplementacionStack<E> implements Stack<E> {
+
+    private Vector<E> stackU;
+
+    public ImplementacionStack (){
+        this.stackU = new Vector<E>();
+    }
+
+    @Override
+    public int size() {
+        return stackU.size();
+    }
+
+    @Override
+    public void push(E item) {
+        this.stackU.add(item);
+    }
+
+    @Override
+    public E peek() {
+        return(stackU.get(stackU.size() - 1)); }
+  
+    @Override
+    public E pop() {
+        return (stackU.remove(stackU.size() - 1));}
+       
+    @Override
+    public boolean empty() {
+        return stackU.isEmpty();
+    }
+
+    
+ 
+    } 
+    
